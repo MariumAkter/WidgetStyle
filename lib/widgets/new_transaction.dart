40418375f42f1a0a81bf4c66fb01ae9e246381ff
Mariumchonmon <Mariumakterch@gmail.com>
2021-06-09 02:05:40 +0600

    Cupertino

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../widgets/adaptive_button.dart';
 import 'package:intl/intl.dart';
 
 
@@ -13,7 +17,7 @@ class NewTransaction extends StatefulWidget {
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
-  late DateTime  _selectedDate;
+  late DateTime  _selectedDate = DateTime.now();
 
   void _submitData () {
     if (_amountController.text.isEmpty){
@@ -52,64 +56,61 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-           elevation: 5,
-           child: Container(
-             padding: EdgeInsets.all(10),
-             child: Column(
-               crossAxisAlignment: CrossAxisAlignment.end,
-               children: <Widget> [
-                 TextField(decoration: InputDecoration(labelText: 'Title'),
-                 controller: _titleController,
-                 onSubmitted: (_)=> _submitData(),
-                 //  onChanged: (val)=> titleInput = val,
-                 ),
-                 TextField(decoration: InputDecoration(labelText: 'Amount'),
-                 controller: _amountController,
-                 keyboardType: TextInputType.number,
-                 onSubmitted: (_) => _submitData(),
-                 //  onChanged: (val)=> amountInput = val,
-               //ignore: deprecated_member_use
-                 ),
-               Container(
-                 height: 70,
-                 child: Row(
-                   children: <Widget> [
-                     Expanded(
-                       child: Text(
-                         // ignore: unnecessary_null_comparison
-                         _selectedDate == null
-                             ? "No Date Chosen!"
-                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
-                       ),
-                     ),
-                     // ignore: deprecated_member_use
-                     FlatButton(
-                       textColor: Theme.of(context).primaryColor,
-                       child: Text('Chosen Date',
-                         style: TextStyle(
-                             fontWeight: FontWeight.bold
+    return SingleChildScrollView(
+      child: Card(
+             elevation: 5,
+             child: Container(
+               padding: EdgeInsets.only(
+                   top: 10,
+                   right: 10,
+                   left: 10,
+                   bottom: MediaQuery.of(context).viewInsets.bottom + 10
+               ),
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.end,
+                 children: <Widget> [
+                   TextField(decoration: InputDecoration(labelText: 'Title'),
+                   controller: _titleController,
+                   onSubmitted: (_)=> _submitData(),
+                   //  onChanged: (val)=> titleInput = val,
+                   ),
+                   TextField(decoration: InputDecoration(labelText: 'Amount'),
+                   controller: _amountController,
+                   keyboardType: TextInputType.number,
+                   onSubmitted: (_) => _submitData(),
+                   //  onChanged: (val)=> amountInput = val,
+                 //ignore: deprecated_member_use
+                   ),
+                 Container(
+                   height: 70,
+                   child: Row(
+                     children: <Widget> [
+                       Expanded(
+                         child: Text(
+                           // ignore: unnecessary_null_comparison
+                           _selectedDate == null
+                               ? "No Date Chosen!"
+                               : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                          ),
-                       ) ,
+                       ),
+                       //ignore: deprecated_member_use
+                      AdaptiveButton('Chose Date', _presentDatePicker)
 
-
-                     onPressed: _presentDatePicker,
-                     ),
-
-                   ],
+                     ],
+                   ),
                  ),
-               ),
-               // ignore: deprecated_member_use
-               RaisedButton(
-                 child: Text('Add Transaction') ,
-                 color: Theme.of(context).primaryColor,
-                 textColor: Theme.of(context).textTheme.button!.color,
-                 onPressed: _submitData,
-               ),
+                 // ignore: deprecated_member_use
+                 RaisedButton(
+                   child: Text('Add Transaction') ,
+                   color: Theme.of(context).primaryColor,
+                   textColor: Theme.of(context).textTheme.button!.color,
+                   onPressed: _submitData,
+                 ),
 
-               ],
+                 ],
+               ),
              ),
            ),
-         );
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,26 +11,27 @@ class TransactionList extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-    child: transactions.isEmpty
-        ? Column(
-      children: <Widget> [
-      Text(
-        'No transactions added yet!',
-        style: Theme.of(context).textTheme.title,
-      ),
-      SizedBox(
-        height: 20,
-      ),
-      Container(
-        height: 200,
-          child: Image.asset(
-              'assets/images/waiting.png',
-               fit: BoxFit.cover)),
-      
-    ],
-    ) 
+    return  transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+          return  Column(
+            children: <Widget> [
+              Text(
+                'No transactions added yet!',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover),
+              ),
+
+            ],
+          );
+    })
     : ListView.builder(
       itemBuilder: (ctx, index){
         return Card(
@@ -57,16 +58,22 @@ class TransactionList extends StatelessWidget {
             subtitle: Text(
               DateFormat.yMMMd().format(transactions[index].date),
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete), color: Theme.of(context).errorColor,
-              onPressed: () => deleteTx(transactions[index].id),
+            trailing: MediaQuery.of(context).size.width > 460
+                ? FlatButton.icon(
+                       icon: Icon(Icons.delete),
+                        label: Text('Delete'),
+                        textColor: Theme.of(context).errorColor ,
+                        onPressed: () => deleteTx(transactions[index].id),
+            )
+                : IconButton(
+                     icon: Icon(Icons.delete),
+                     color: Theme.of(context).errorColor,
+                     onPressed: () => deleteTx(transactions[index].id),
             ),
           ),
         );
       },
       itemCount: transactions.length,
-
-             ),
     );
   }
 }
